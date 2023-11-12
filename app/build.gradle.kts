1d3e5bc77ff42bde340294801ecd8e1a3218114d
Y_Nishino <kaek33586@gmail.com>
2023-11-12 22:08:13 +0900

    add camera screen

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,7 +53,7 @@ android {
 
 dependencies {
 
-
+    //Jetpack Composeの依存関係
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
     implementation("androidx.activity:activity-compose:1.8.0")
@@ -65,7 +65,17 @@ dependencies {
     implementation("androidx.navigation:navigation-compose:2.7.5")
     implementation("androidx.compose.material:material-icons-extended:1.5.4")
 
+    //Firebaseの依存関係
     implementation(platform("com.google.firebase:firebase-bom:32.5.0"))
+
+    //カメラの依存関係
+    val camerax = "1.4.0-alpha02"
+    implementation ("androidx.camera:camera-core:${camerax}")
+    implementation ("androidx.camera:camera-camera2:${camerax}")
+    implementation ("androidx.camera:camera-lifecycle:${camerax}")
+    implementation ("androidx.camera:camera-video:${camerax}")
+    implementation ("androidx.camera:camera-view:${camerax}")
+    implementation ("androidx.camera:camera-extensions:${camerax}")
 
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
--- app/src/main/java/com/kotlincocktail/pourpal/navigation/navigation.kt
+++ app/src/main/java/com/kotlincocktail/pourpal/navigation/navigation.kt
@@ -1,21 +1,31 @@
 package com.kotlincocktail.pourpal.navigation
 
+import android.content.Context
+import androidx.camera.view.CameraController
+import androidx.camera.view.LifecycleCameraController
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import com.kotlincocktail.pourpal.views.main
+import com.kotlincocktail.pourpal.views.CameraView
 
 @Composable
-fun Navigation() {
+fun Navigation(applicationContext: Context) {
     val navController = rememberNavController()
+
+
+
     NavHost(
         navController = navController,
         startDestination = "main"
     ) {
 
         composable(route = "main") {
-            main(navController=navController)
+            CameraView(
+                navController=navController,
+                applicationContext = applicationContext
+            )
         }
 //メモ
 //        composable(route = "page2") {
