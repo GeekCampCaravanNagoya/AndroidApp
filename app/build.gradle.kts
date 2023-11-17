4c7b1b09274a15df7e44676a94a95147ead5b5a2
Hiroto2002 <hiroto2002h14@icloud.com>
2023-11-17 20:23:36 +0900

    fix/gradleBug

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -84,15 +84,6 @@ dependencies {
     //Firebaseの依存関係
     implementation(platform("com.google.firebase:firebase-bom:32.5.0"))
 
-    //カメラの依存関係
-    val camerax = "1.4.0-alpha02"
-    implementation ("androidx.camera:camera-core:${camerax}")
-    implementation ("androidx.camera:camera-camera2:${camerax}")
-    implementation ("androidx.camera:camera-lifecycle:${camerax}")
-    implementation ("androidx.camera:camera-video:${camerax}")
-    implementation ("androidx.camera:camera-view:${camerax}")
-    implementation ("androidx.camera:camera-extensions:${camerax}")
-
     val room_version = "2.5.0"
 
     implementation("androidx.room:room-runtime:$room_version")
