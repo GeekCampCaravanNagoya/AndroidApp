97e801b6e1333f5cce00b12a7e3ceedd8267c0d3
Y_Nishino <kaek33586@gmail.com>
2023-11-17 14:32:08 +0900

    add title and back page button

--- app/src/main/java/com/kotlincocktail/pourpal/views/CameraView.kt
+++ app/src/main/java/com/kotlincocktail/pourpal/views/CameraView.kt
@@ -19,14 +19,17 @@ import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowBackIosNew
 import androidx.compose.material.icons.filled.Collections
+import androidx.compose.material.icons.outlined.Close
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.Button
@@ -37,7 +40,6 @@ import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
-import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
@@ -45,6 +47,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Alignment.Companion.Center
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -52,6 +55,7 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.core.content.ContextCompat
 import androidx.navigation.NavHostController
@@ -75,29 +79,15 @@ fun CameraView(navController: NavHostController) {
     }
 
     Scaffold(
-        topBar = {
-            TopAppBar(
-                title = { Text(text = "メニューを撮影") },
-                navigationIcon = {
-                    Icon(
-                        imageVector = Icons.Filled.ArrowBackIosNew,
-                        contentDescription = "",
-                        modifier = Modifier.clickable{
-                            navController.navigateUp()
-                        }
-                    )
-                }
-            )
-        },
         bottomBar = {
-            BottomAppBar {
+            BottomAppBar(Modifier.height(100.dp)) {
                 Row(
                     modifier = Modifier
                         .fillMaxWidth(),
                     horizontalArrangement = Arrangement.SpaceBetween
                 ) {
-                    Spacer(modifier = Modifier.weight(1f))
-                    Box(modifier = Modifier.weight(1f)) {
+                    Spacer(modifier = Modifier.width(60.dp))
+                    Box(Modifier.padding(8.dp)) {
                         Image(modifier = Modifier.align(Center), painter = painterResource(id = R.drawable.shutter1), contentDescription = "")
                         Image(modifier = Modifier
                             .align(Center)
@@ -113,10 +103,14 @@ fun CameraView(navController: NavHostController) {
                     }
                     IconButton(
                         modifier = Modifier
-                            .weight(1f)
+                            .width(60.dp)
                             .fillMaxHeight(),
                         onClick = { showBottomSheet = true }) {
-                        Icon(imageVector = Icons.Filled.Collections, contentDescription = "Show Gallery")
+                        Icon(
+                            modifier = Modifier.size(30.dp),
+                            imageVector = Icons.Filled.Collections,
+                            contentDescription = "Show Gallery"
+                        )
                     }
                 }
             }
@@ -132,16 +126,25 @@ fun CameraView(navController: NavHostController) {
                                 controller.bindToLifecycle(lifecycleOwner)
                             }
                         },
-                        modifier = Modifier.fillMaxSize()
+                        modifier = Modifier
+                            .fillMaxSize()
                     )
                 permissionState.shouldShowRationale -> PermissionRationaleDialog {
                     permissionState.launchPermissionRequest()
                 }
-                permissionState.permissionRequested -> Text("カメラを使わせてくれ")
+                permissionState.permissionRequested -> Text("カメラを使わせてくれ",Modifier.align(Center))
                 else -> SideEffect {
                     permissionState.launchPermissionRequest()
                 }
             }
+            IconButton(
+                onClick = { navController.navigateUp() },
+            ) {
+                Icon(Icons.Outlined.Close,"",
+                    modifier = Modifier.size(30.dp)
+                )
+            }
+            Text(text = "メニューを撮影", modifier = Modifier.align(Alignment.TopCenter).padding(top = 8.dp), fontSize = 18.sp)
             // TODO(AddCameraView)
             if (showBottomSheet) {
                 ModalBottomSheet(
